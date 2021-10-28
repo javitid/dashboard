16ca36c8182a20717415dfbb4a3e7e4ee7cb81f7
Javier Garcia <javitid@gmail.com>
2021-10-28 23:45:49 +0200

    Add left menu with styling

---
+++ index.js
@@ -0,0 +1,13 @@
+// Add hoved class in selected menu element
+let list = document.querySelectorAll('.navigation li');
+
+function activeLink() {
+    list.forEach(item => {
+        item.classList.remove('hovered');
+        this.classList.add('hovered');
+    })
+}
+
+list.forEach(item => {
+    item.addEventListener('mouseover', activeLink);
+});
